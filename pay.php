3417fad76e9c41c54856d3eabb4df75a5e63b65c
jauntyjay09 <49561283+jauntyjay09@users.noreply.github.com>
2020-06-19 18:40:14 +0530

    Add files via upload

---
+++ pay.php
@@ -0,0 +1,105 @@
+<script type="text/javascript">
+        function preventBack() {
+            window.history.forward();
+        }
+
+        setTimeout("preventBack()", 0);
+
+        window.onunload = function () { null };
+    </script><script type="text/javascript">
+        function preventBack() {
+            window.history.forward();
+        }
+
+        setTimeout("preventBack()", 0);
+
+        window.onunload = function () { null };
+    </script>
+<?php
+
+require('config.php');
+require('razorpay-php/Razorpay.php');
+session_start();
+
+// Create the Razorpay Order
+$amount_IN=$_SESSION['amount_INR'];
+$discount=$_SESSION['discount'];
+$course_name='IndustryReady Master Class';
+$name=$_SESSION['name'];
+$email=$_SESSION['email'];
+$receipt=$_SESSION['receipt'];
+$phone = $_SESSION['phone'];
+$amount_INR=$amount_IN - $discount;
+$college=$_SESSION['college'];
+$branch=$_SESSION['branch'];
+$sem=$_SESSION['sem'];
+$city=$_SESSION['city'];
+$referral=$_SESSION['referral'];
+use Razorpay\Api\Api;
+
+$api = new Api($keyId, $keySecret);
+
+//
+// We create an razorpay order using orders api
+// Docs: https://docs.razorpay.com/docs/orders
+//
+$orderData = [
+    'receipt'         => $receipt,
+    'amount'          => $amount_INR * 100, // 2000 rupees in paise
+    'currency'        => 'INR',
+    'payment_capture' => 1 // auto capture
+];
+
+$razorpayOrder = $api->order->create($orderData);
+
+$razorpayOrderId = $razorpayOrder['id'];
+
+$_SESSION['razorpay_order_id'] = $razorpayOrderId;
+
+$displayAmount = $amount = $orderData['amount'];
+
+if ($displayCurrency !== 'INR')
+{
+    $url = "https://api.fixer.io/latest?symbols=$displayCurrency&base=INR";
+    $exchange = json_decode(file_get_contents($url), true);
+
+    $displayAmount = $exchange['rates'][$displayCurrency] * $amount / 100;
+}
+
+$checkout = 'automatic';
+
+if (isset($_GET['checkout']) and in_array($_GET['checkout'], ['automatic', 'manual'], true))
+{
+    $checkout = $_GET['checkout'];
+}
+
+$data = [
+    "key"               => $keyId,
+    "amount"            => $amount,
+    "name"              => "Protrainy",
+    "description"       => "IndustryReady Master Class",
+    "image"             => "https://www.protrainy.com/assets/Images/logo2.png",
+    "prefill"           => [
+    "name"              => $name,
+    "email"             => $email,
+    "contact"           => $phone,
+    ],
+    "notes"             => [
+    "address"           => "NA",
+    "merchant_order_id" => $receipt,
+    ],
+    "theme"             => [
+    "color"             => "#F37254"
+    ],
+    "order_id"          => $razorpayOrderId,
+];
+
+if ($displayCurrency !== 'INR')
+{
+    $data['display_currency']  = $displayCurrency;
+    $data['display_amount']    = $displayAmount;
+}
+
+$json = json_encode($data);
+
+require("checkout/{$checkout}.php");
